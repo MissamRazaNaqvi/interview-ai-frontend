ae28d6d861aeb3a9f7c95055f2a16d5afc4c837f
misamraja.nakvi@krishtechnolabs.com <misamraja.nakvi@krishtechnolabs.com>
2026-06-29 19:53:40 +0530

    issue fix

--- src/main.jsx
+++ src/main.jsx
@@ -4,17 +4,15 @@ import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from "react-router-dom";
 import { UserProvider } from './context/AuthContext.jsx';
-import { SpeedInsights } from "@vercel/speed-insights/next"
-
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
       <UserProvider>
-        <SpeedInsights>
-          <App />
-        </SpeedInsights>
+        <App />
+        <SpeedInsights />
       </UserProvider>
     </BrowserRouter>
   </StrictMode>,
